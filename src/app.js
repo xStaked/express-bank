eb661ff34883374b660f149f7d2809f62edbd129
xStaked <aljndr911@gmail.com>
2023-06-12 18:39:35 -0500

    feat: add cors middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/v1/auth', authRoutes);
 
