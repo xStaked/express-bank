6f0d137ea4dae0d168b77f0c7c520b082ac0cfeb
xStaked <aljndr911@gmail.com>
2023-06-12 17:41:59 -0500

    add user model

---
+++ src/models/user.model.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const { db } = require('../database/config');
+const User = db.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+    },
+    accountNumber: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+    },
+    amount: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+});
+module.exports = User;
